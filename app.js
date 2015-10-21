b420862a3e8c6be989b1ffbc9a80330610a23c37
alexnicolini <alexnicolini@gmail.com>
2015-10-21 21:55:28 -0200

    Create AngularJS module and add angular-meteor dependency

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 if (Meteor.isClient) {
-  
+  // This code only run on the client side
+
+  // Create AngularJS module and add angular-meteor dependency
+  angular.module('socially', ['angular-meteor']);
 }
