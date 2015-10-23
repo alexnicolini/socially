7f366c27d2b5f19a49d2d7a38ebbdaa3e999da1c
alexnicolini <alexnicolini@gmail.com>
2015-10-23 14:23:36 -0200

    Use remove function instead of splice

--- app.ng.js
+++ app.ng.js
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
     $scope.parties = $meteor.collection(Parties);
 
     $scope.remove = function(party) {
-      $scope.parties.splice($scope.parties.indexOf(party), 1);
+      $scope.parties.remove(party);
     }
   });
 }
